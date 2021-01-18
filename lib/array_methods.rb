1166cec074cd750bfba894235f21e171ac9a90f8
Minyoung Shin <mins6649@gmail.com>
2021-01-18 18:30:52 -0500

    commit tree 95b804d85dd665eb88ccb8f07675641a6974fa15

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,41 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  counter = 0
+
+  while counter < array.length do 
+    if array[counter] == value_to_find 
+      return counter
+    end
+    counter += 1
+
+  end
 end
 
-def find_max_value(array)
-  # Add your solution here
+def find_max_value(array)  
+# identify the largest number
+# check every number in the array
+# have a counter that checks every index
+
+counter = 0
+max = array[0]
+
+while counter < array.length do
+  if array[counter] > max 
+    max = array[counter]
+  end
+counter += 1
+end
+return max
 end
 
 def find_min_value(array)
-  # Add your solution here
+  counter = 0
+min = array[0]
+
+while counter < array.length do
+  if array[counter] < min 
+    min = array[counter]
+  end
+counter += 1
+end
+return min
 end
